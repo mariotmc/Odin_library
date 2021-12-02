3b0e8b2c854de12ae2865c483b0a586591521fbe
mariotmc <mariotomicx@gmail.com>
2021-12-02 18:41:06 +0100

    Add functionality and design

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
-const newBookButton = document.querySelector("#new-book");
+const addBookButton = document.querySelector("#add-book");
 const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
 const submitButton = document.querySelector("#submit");
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
@@ -19,8 +20,8 @@ function toggleFormVisibility() {
   }
 }
 
-newBookButton.addEventListener("click", () => {
-  toggleFormVisibility();
+addBookButton.addEventListener("click", () => {
+  modal.style.display = "block";
 });
 
 function Book() {
@@ -36,49 +37,73 @@ function Book() {
 function displayBooks() {
   library.forEach((element) => {
     let book = document.createElement("div");
+    book.classList.add("book");
 
     let title = document.createElement("h3");
     let titleText = document.createTextNode(element.title);
     title.appendChild(titleText);
 
     let author = document.createElement("p");
-    let authorText = document.createTextNode(`Author: ${element.author}`);
+    let authorText = document.createTextNode(element.author);
     author.appendChild(authorText);
 
     let pages = document.createElement("p");
-    let pagesText = document.createTextNode(`Pages: ${element.pages}`);
+    let pagesText = document.createTextNode(`${element.pages} Pages`);
     pages.appendChild(pagesText);
-
-    let read = document.createElement("p");
-    let readText = document.createTextNode(`Read: ${element.read}`);
-    read.appendChild(readText);
 
     let readStatus = document.createElement("button");
     let readStatusText = document.createTextNode("Read");
+    readStatus.classList.add("read-status__button");
     readStatus.appendChild(readStatusText);
 
     let removeBook = document.createElement("button");
     let removeBookText = document.createTextNode("Remove");
+    removeBook.classList.add("remove-book__button");
     removeBook.appendChild(removeBookText);
+
+    let buttons = document.createElement("div");
+    buttons.classList.add("buttons-container");
+    buttons.appendChild(readStatus);
+    buttons.appendChild(removeBook);
 
     book.appendChild(title);
     book.appendChild(author);
     book.appendChild(pages);
-    book.appendChild(read);
-    book.appendChild(readStatus);
-    book.appendChild(removeBook);
+    book.appendChild(buttons);
     container.appendChild(book);
 
+    if (element.read === true) {
+      readStatus.style.backgroundColor = "#1d532c";
+      readStatus.textContent = "Read";
+    }
+    if (element.read === false) {
+      readStatus.style.backgroundColor = "#222425";
+      readStatus.textContent = "Not read";
+    }
+
     readStatus.addEventListener("click", () => {
-      if (read.textContent === "Read: Yes") {
-        read.textContent = "Read: No";
-      } else if (read.textContent === "Read: No") {
-        read.textContent = "Read: Yes";
+      if (element.read === true) {
+        element.read = false;
+        readStatus.textContent = "Not read";
+        readStatus.style.backgroundColor = "#222425";
+      } else if (element.read === false) {
+        element.read = true;
+        readStatus.textContent = "Read";
+        readStatus.style.backgroundColor = "#1d532c";
       }
     });
 
-    removeBook.addEventListener("click", () => {
-      container.removeChild(book);
+    removeBook.addEventListener("click", (e) => {
+      library.map((element) => {
+        if (
+          element.title ===
+          e.target.parentElement.parentElement.firstChild.textContent
+        ) {
+          let index = library.indexOf(element);
+          library.splice(index, 1);
+          container.removeChild(book);
+        }
+      });
     });
   });
 }
@@ -87,22 +112,20 @@ submitButton.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (!titleInput.value) {
-    alert("Please enter a title");
+    return;
   } else if (!authorInput.value) {
-    alert("Please enter an author");
+    return;
   } else if (!pagesInput.value) {
-    alert("Please enter the number of pages");
-  } else if (!pagesInput.value) {
-    alert("Please enter if you have read the book");
+    return;
   } else {
     const NewBook = Object.create(Book);
     NewBook.title = titleInput.value;
     NewBook.author = authorInput.value;
     NewBook.pages = pagesInput.value;
     if (readInput.checked) {
-      NewBook.read = "Yes";
+      NewBook.read = true;
     } else if (!readInput.checked) {
-      NewBook.read = "No";
+      NewBook.read = false;
     }
 
     library.push(NewBook);
@@ -111,6 +134,7 @@ submitButton.addEventListener("click", (e) => {
   toggleFormVisibility();
   clearContainer();
   displayBooks();
+  submitButton.style.backgroundColor = "#782729";
 });
 
 function clearContainer() {
@@ -118,6 +142,12 @@ function clearContainer() {
     container.removeChild(container.firstChild);
   }
 }
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    submitButton.click();
+  }
+});
 
 function clearInput() {
   titleInput.textContent = "";
@@ -131,8 +161,27 @@ function clearInput() {
   readInput.checked = false;
 }
 
-body.addEventListener("click", () => {
-  if ((modal.style.display = "block")) {
-    modal.style.display = "none";
+document.addEventListener("input", () => {
+  if (titleInput.value && authorInput.value && pagesInput.value) {
+    submitButton.style.backgroundColor = "#1d532c";
+  }
+  if (!titleInput.value || !authorInput.value || !pagesInput.value) {
+    submitButton.style.backgroundColor = "#782729";
+  }
+});
+
+submitButton.addEventListener("mouseenter", () => {
+  submitButton.style.filter = "brightness(130%)";
+});
+
+submitButton.addEventListener("mouseleave", () => {
+  submitButton.style.filter = "brightness(100%)";
+});
+
+document.addEventListener("mouseup", (e) => {
+  if (modal.style.display === "block") {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
   }
 });
